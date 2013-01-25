60532456cd9c65308909cfde1c982c8a98a2dcc3
MiloserdOFF <milos-plus@live.ru>
2013-01-25 23:50:28 +0700

    Сделана панель вкладок. Имеются баги.

---
+++ Appendix.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-01-25T22:06:03
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Appendix
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ mainwindow.cpp
@@ -0,0 +1,51 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <string.h>
+#include <stdio.h>
+
+
+int n=0,cur_tab=0;
+QTextEdit *tabs[10];
+
+QString strtoint(int a)
+{
+    char n[100];
+    sprintf(n,"%d",a);
+    return QString(n);
+}
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_tabWidget_tabCloseRequested(int index)
+{
+    delete tabs[index+1];
+    cur_tab--;
+    n--;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    n++;
+    tabs[n]= new QTextEdit;
+    ui->tabWidget->addTab(tabs[n],QString("Tab")+QString(strtoint(n)));
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->textEdit->setText(tabs[cur_tab]->toPlainText());
+}
+
+void MainWindow::on_tabWidget_currentChanged(int index)
+{
+    cur_tab=index+1;
+}
